7e4badd8868e6044c59318da831f025d545c327e
Sergei Shtepa <sergei.shtepa@veeam.com>
2021-09-30 10:31:15 +0200

    some improvements in mk.sh

--- module/mk.sh
+++ module/mk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # SPDX-License-Identifier: GPL-2.0
-KERNEL_MODULE_NAME=blk-snap
+MODULE_NAME=blk-snap
 CMD=$1
 if [ -n "$2" ]
 then
@@ -13,6 +13,7 @@ then
 else
 	KERNEL_RELEASE="$(uname -r)"
 fi
+MODULE_PATH=/lib/modules/${KERNEL_RELEASE}/kernel/drivers/block
 
 case "$CMD" in
 	build)
@@ -27,21 +28,21 @@ case "$CMD" in
 		;;
 	install)
 		echo Installing ...
-		mkdir -p /lib/modules/${KERNEL_RELEASE}/kernel/drivers
-		cp ${KERNEL_MODULE_NAME}.ko /lib/modules/${KERNEL_RELEASE}/kernel/drivers
+		mkdir -p ${MODULE_PATH}
+		cp ${MODULE_NAME}.ko ${MODULE_PATH}
 		depmod
 		echo Completed.
 		;;
 	uninstall)
-		rm -f /lib/modules/${KERNEL_RELEASE}/kernel/drivers/${KERNEL_MODULE_NAME}.ko
+		rm -f ${MODULE_PATH}/${MODULE_NAME}.ko
 		;;
 	load)
-		echo "Loading ${KERNEL_MODULE_NAME} kernel module from current folder"
-		insmod ./${KERNEL_MODULE_NAME}.ko zerosnapdata=1
+		echo "Loading ${MODULE_NAME} kernel module from current folder"
+		insmod ./${MODULE_NAME}.ko
 		;;
 	unload)
-		echo "Unloading ${KERNEL_MODULE_NAME} kernel module"
-		rmmod ${KERNEL_MODULE_NAME}
+		echo "Unloading ${MODULE_NAME} kernel module"
+		rmmod ${MODULE_NAME}
 		;;
 	*)
 		echo "usage: $0 {build | clean | install | uninstall | load | unload}"
