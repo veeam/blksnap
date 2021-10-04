96a4798380171001248d6c48a060cb199b9cea00
Sergei Shtepa <sergei.shtepa@veeam.com>
2021-10-04 14:20:53 +0200

    module build and install steps moved to separated script

--- tests/bash/all.sh
+++ tests/bash/all.sh
@@ -1,14 +1,6 @@
 #!/bin/bash -e
 
-TEST_ALL_PWD=$(pwd)
-echo "Current directory ${TEST_ALL_PWD}"
-echo "Complete and install module"
-cd ${TEST_ALL_PWD}/../../module
-./mk.sh build
-./mk.sh install
-cd ${TEST_ALL_PWD}
-
-echo "Current directory ${TEST_ALL_PWD}"
+./build_and_install.sh
 
 echo "Execute all tests"
 echo "***"
---
+++ tests/bash/build_and_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+TEST_ALL_PWD=$(pwd)
+
+echo "Build and install module"
+cd ${TEST_ALL_PWD}/../../module
+./mk.sh build
+./mk.sh install
+
+cd ${TEST_ALL_PWD}
