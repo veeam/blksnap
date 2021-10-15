4b468aa7cb1ad99b5bdd979b6a94bec256fa1100
Sergei Shtepa <sergei.shtepa@veeam.com>
2021-10-15 15:55:59 +0200

    set version 1.0.0.0

--- module/version.h
+++ module/version.h
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #pragma once
 
-#define FILEVER_MAJOR 6
+#define FILEVER_MAJOR 1
 #define FILEVER_MINOR 0
 #define FILEVER_REVISION 0
 #define FILEVER_BUILD 0
-#define FILEVER_STR "6.0.0.0"
+#define FILEVER_STR "1.0.0.0"
