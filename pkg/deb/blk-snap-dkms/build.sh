da3941da84e02a9a68e910d27c4411985f7a4039
Sergei Shtepa <sergei.shtepa@veeam.com>
2021-10-19 15:06:00 +0200

    use generate_version() from separated file

--- pkg/deb/blk-snap-dkms/build.sh
+++ pkg/deb/blk-snap-dkms/build.sh
@@ -1,23 +1,5 @@
 #!/bin/bash -e
 
-generate_version()
-{
-	local FILEPATH=$1
-	local VER_STR=$2
-	local VER_MAJOR=$(cut -d "." -f1 <<< ${VER_STR})
-	local VER_MINOR=$(cut -d "." -f2 <<< ${VER_STR})
-	local VER_REVIS=$(cut -d "." -f3 <<< ${VER_STR})
-	local VER_BUILD=$(cut -d "." -f4 <<< ${VER_STR})
-
-	echo "/* SPDX-License-Identifier: GPL-2.0 */" > ${FILEPATH}
-	echo "#pragma once" >> ${FILEPATH}
-	echo "#define VERSION_MAJOR	${VER_MAJOR}" >> ${FILEPATH}
-	echo "#define VERSION_MINOR	${VER_MINOR}" >> ${FILEPATH}
-	echo "#define VERSION_REVISION	${VER_REVIS}" >> ${FILEPATH}
-	echo "#define VERSION_BUILD	${VER_BUILD}" >> ${FILEPATH}
-	echo "#define VERSION_STR	\"${VER_STR}\"" >> ${FILEPATH}
-}
-
 VERSION="$1"
 if [ -n "$1" ]
 then
@@ -27,20 +9,25 @@ else
 fi
 
 CURR_DIR=$(pwd)
-BUILD_DIR="build/pkg"
-
 cd "../../../"
 
+. pkg/build_functions.sh
+
+BUILD_DIR="build/pkg"
+rm -rf ${BUILD_DIR}
+
+# prepare module sources
 mkdir -p ${BUILD_DIR}/src
 cp module/*.c ${BUILD_DIR}/src/
 cp module/*.h ${BUILD_DIR}/src/
 cp module/Makefile* ${BUILD_DIR}/src/
+generate_version ${BUILD_DIR}/src/version.h ${VERSION}
 
+# prepare other package files
 cp -r pkg/deb/blk-snap-dkms/debian ${BUILD_DIR}
+cp pkg/blk-snap.dkms ${BUILD_DIR}/
 chmod 0666 ${BUILD_DIR}/debian/control
 chmod 0766 ${BUILD_DIR}/debian/rules
-
-generate_version ${BUILD_DIR}/src/version.h ${VERSION}
 find ${BUILD_DIR} -type f -exec sed -i 's/#PACKAGE_VERSION#/'${VERSION}'/g' {} +
 
 cd ${BUILD_DIR} && dpkg-buildpackage -us -uc -b
