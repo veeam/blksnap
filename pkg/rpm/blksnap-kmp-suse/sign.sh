da933e7e31b715db697911e8f29c5ef4b4630584
Sergei Shtepa <sergei.shtepa@veeam.com>
2021-12-07 13:43:17 +0100

    fix sign.sh script

--- pkg/rpm/blksnap-kmp-suse/sign.sh
+++ pkg/rpm/blksnap-kmp-suse/sign.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -e
 
+PROJECT_DIR=$(pwd)
 BUILD_DIR="${HOME}/rpmbuild"
 
 # signing only if private key exist in current directory
-if [ -f ./signing_key.pem ]
+if [ not -f ${PROJECT_DIR}/signing_key.pem ]
 then
 	echo "The current directory must contain a public 'signing_key.x509' and private 'signing_key.pem' keys."
 	exit 1;
 fi
 
-for PKG in ${BUILD_DIR}/RPMS/*/blksnap-kmp*.rpm
+cd ${BUILD_DIR}
+for PKG in ./RPMS/*/blksnap-kmp*.rpm
 do
-	modsign-repackage -c ./signing_key.x509 -k ./signing_key.pem ${PKG}
+	modsign-repackage -c ${PROJECT_DIR}/signing_key.x509 -k ${PROJECT_DIR}/signing_key.pem ${PKG}
 done
+cd ${PROJECT_DIR}
 exit 0
