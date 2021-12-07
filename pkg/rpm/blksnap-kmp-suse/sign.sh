66ca18c0f8c7c11b3a2597a7476accfbb7cfd9d9
Sergei Shtepa <sergei.shtepa@veeam.com>
2021-12-07 17:42:02 +0100

    move keys to ${HOME}/cert/

--- pkg/rpm/blksnap-kmp-suse/sign.sh
+++ pkg/rpm/blksnap-kmp-suse/sign.sh
@@ -4,16 +4,16 @@ PROJECT_DIR=$(pwd)
 BUILD_DIR="${HOME}/rpmbuild"
 
 # signing only if private key exist in current directory
-if [ not -f ${PROJECT_DIR}/signing_key.pem ]
+if [ not -f ${HOME}/cert/signing_key.pem ]
 then
-	echo "The current directory must contain a public 'signing_key.x509' and private 'signing_key.pem' keys."
+	echo "The directory '${HOME}/cert/' must contain a public 'signing_key.x509' and private 'signing_key.pem' keys."
 	exit 1;
 fi
 
 cd ${BUILD_DIR}
 for PKG in ./RPMS/*/blksnap-kmp*.rpm
 do
-	modsign-repackage -c ${PROJECT_DIR}/signing_key.x509 -k ${PROJECT_DIR}/signing_key.pem ${PKG}
+	modsign-repackage -c ${HOME}/cert/signing_key.x509 -k ${HOME}/cert/signing_key.pem ${PKG}
 done
 cd ${PROJECT_DIR}
 exit 0
