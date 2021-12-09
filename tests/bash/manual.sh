4bf0a005bfa845b66c35ac599d12a2acdebd1fc4
Sergei Shtepa <sergei.shtepa@veeam.com>
2021-12-09 15:53:59 +0100

    add manual test
    A manual test allows to create a snapshot and conduct some kind of
    experiment in manual mode. Then close the snapshot and release the used
    resources.

---
+++ tests/bash/manual.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+
+. ./functions.sh
+. ./blksnap.sh
+
+echo "---"
+echo "Manual test start"
+
+DEVICE=$1
+if [ -z ${DEVICE} ]
+then
+	echo "Should be set device ${DEVICE} as first parameter"
+	exit 1
+fi
+echo "Text with experiment device ${DEVICE}"
+
+DIFF_STORAGE=$2
+if [ -z ${DIFF_STORAGE} ]
+then
+	echo "Should be set difference storage ${DIFF_STORAGE} as second parameter"
+	exit 1
+fi
+echo "Difference will be storage on ${DIFF_STORAGE}"
+
+DIFF_STORAGE_SIZE="1GiB"
+echo "Difference storage size ${DIFF_STORAGE_SIZE}"
+
+# check module is ready
+blksnap_version
+
+echo "Block device prepared, press ..."
+read -n 1
+
+blksnap_snapshot_create "${DEVICE}"
+
+DIFF_STORAGE=${DIFF_STORAGE}/diff_storage0
+rm -f ${DIFF_STORAGE}/diff_storage0
+fallocate --length ${DIFF_STORAGE_SIZE} ${DIFF_STORAGE}
+blksnap_snapshot_append ${DIFF_STORAGE}
+
+blksnap_snapshot_take
+echo "Snapshot was token"
+
+blksnap_snapshot_collect_all
+echo "Make your manual tests, then press ..."
+read -n 1
+
+echo "Destroy snapshot"
+blksnap_snapshot_destroy
+
+rm ${DIFF_STORAGE}
+
+echo "Manual test finish"
+echo "---"
