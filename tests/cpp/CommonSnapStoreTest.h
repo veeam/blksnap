063030f0c74ef2ee10fc8bfc6fca10f96fa915e1
sshtepa <sergei.shtepa@veeam.com>
2021-12-23 17:11:05 +0100

    remove some deprecated files

--- tests/cpp/CommonSnapStoreTest.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <blk-snap-cpp/BlkSnapStoreCtx.h>
-#include <boost/filesystem.hpp>
-
-void CommonSnapStoreTest(boost::filesystem::path testDir, boost::filesystem::path originalDev,
-                         BlkSnapStoreCtx& storeCtx);
