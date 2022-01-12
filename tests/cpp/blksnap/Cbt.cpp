a0c69c2dc9a5276d1cc01da9f073fe6b351529c6
sshtepa <sergei.shtepa@veeam.com>
2022-01-12 13:56:55 +0100

    add includes

--- tests/cpp/blksnap/Cbt.cpp
+++ tests/cpp/blksnap/Cbt.cpp
@@ -1,3 +1,7 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/sysmacros.h>
+#include <system_error>
 #include "Cbt.h"
 #include "Blksnap.h"
 
@@ -33,18 +37,18 @@ CCbt::CCbt()
 const struct blk_snap_cbt_info & CCbt::GetCbtInfoInternal(unsigned int mj, unsigned int mn)
 {
     for (const struct blk_snap_cbt_info & cbtInfo : m_cbtInfos)
-        if ((mj == cbtInfo.originalMajor) && (mn == cbtInfo.originalMajor))
+        if ((mj == cbtInfo.dev_id.mj) && (mn == cbtInfo.dev_id.mn))
             return cbtInfo;
 
-    throw std::runtime_error("The device [" + devId.ToString() + "] was not found in the CBT table");
+    throw std::runtime_error("The device [" + std::to_string(mj) + ":" + std::to_string(mn) + "] was not found in the CBT table");
 }
 
 std::shared_ptr<SCbtInfo> CCbt::GetCbtInfo(const std::string &original)
 {
     struct stat st;
 
-    if (::stat(name.c_str(), &st))
-        throw std::system_error(errno, std::generic_category(), name);
+    if (::stat(original.c_str(), &st))
+        throw std::system_error(errno, std::generic_category(), original);
 
     const struct blk_snap_cbt_info &cbtInfo =
         GetCbtInfoInternal(major(st.st_rdev), minor(st.st_rdev));
