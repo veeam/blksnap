82bc7b0b4492806ef39bb7365759043d176117da
Sergei Shtepa <sergei.shtepa@veeam.com>
2022-02-14 11:03:05 +0100

    use tools/ instead /utils/

--- tests/blksnap.sh
+++ tests/blksnap.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-BLKSNAP="$(cd ../../; pwd)/utils/blksnap/bin/blksnap"
+BLKSNAP="$(cd ../../; pwd)/tools/blksnap/bin/blksnap"
 ID=""
 
 blksnap_version()
