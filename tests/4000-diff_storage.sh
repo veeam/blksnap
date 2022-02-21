ce7cdfe54be1b3515a2b17e26ebdb81d59d7cbf1
Sergei Shtepa <sergei.shtepa@veeam.com>
2022-02-21 11:50:29 +0100

    diff storage test looks good

--- tests/4000-diff_storage.sh
+++ tests/4000-diff_storage.sh
@@ -23,7 +23,7 @@
 echo "---"
 echo "Diff storage test"
 
-# diff_storage_minimum=262144 - set 256 K sectors, it's 125MiB dikk_storage portion size
+# diff_storage_minimum=262144 - set 256 K sectors, it's 125MiB diff_storage portion size
 modprobe blksnap diff_storage_minimum=262144
 
 # check module is ready
@@ -31,24 +31,57 @@ blksnap_version
 
 if [ -z $1 ]
 then
-	TESTDIR=~/blksnap-test
+	TEST_DIR=$(realpath ~/blksnap-test)
 else
-	TESTDIR=$1
+	TEST_DIR=$(realpath $1)
 fi
-DEVICE="/dev/block/"$(mountpoint -d $(stat -c %m ~/))
-echo "Test directory [${TESTDIR}] on device [${DEVICE}] selected"
+MP_TEST_DIR=$(stat -c %m ${TEST_DIR})
+DEVICE="/dev/block/"$(mountpoint -d ${MP_TEST_DIR})
+echo "Test directory [${TEST_DIR}] on device [${DEVICE}] selected"
 
-MPDIR=/mnt/blksnap-test
-DIFF_STORAGE=${TESTDIR}/diff_storage/
+RELATIVE_TEST_DIR=${TEST_DIR#${MP_TEST_DIR}}
 
-rm -rf ${TESTDIR}
-rm -rf ${MPDIR}
+MP_DIR=/mnt/blksnap-test
+DIFF_STORAGE=${TEST_DIR}/diff_storage/
+
+rm -rf ${TEST_DIR}
+rm -rf ${MP_DIR}
 rm -rf ${DIFF_STORAGE}
-mkdir -p ${TESTDIR}
-mkdir -p ${MPDIR}
+mkdir -p ${TEST_DIR}
+mkdir -p ${MP_DIR}
 mkdir -p ${DIFF_STORAGE}
 
 
+generate_block_MB ${TEST_DIR} "before" 10
+check_files ${TEST_DIR}
+
+blksnap_snapshot_create "${DEVICE}"
+
+blksnap_stretch_snapshot ${DIFF_STORAGE} 1024
+echo "Waiting for creating first portion"
+sleep 2s
+
+blksnap_snapshot_take
+
+generate_block_MB ${TEST_DIR} "after" 1000
+check_files ${TEST_DIR}
+
+IMAGE=${MP_DIR}/image0
+mkdir -p ${IMAGE}
+
+echo "Mount image"
+mount -o nouuid /dev/blksnap-image0 ${IMAGE}
+
+check_files ${IMAGE}/${RELATIVE_TEST_DIR}
+
+echo "Umount image"
+umount ${IMAGE}
+
+echo "Destroy snapshot"
+blksnap_snapshot_destroy
+
+echo "Waiting for streach process terminate"
+sleep 2s
 
 echo "Unload module"
 modprobe -r blksnap
