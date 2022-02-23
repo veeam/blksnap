2a1374c70a5eb6032db7253e3f73cd00a543c7e4
Sergei Shtepa <sergei.shtepa@veeam.com>
2022-02-23 16:18:12 +0100

    added logging of raw data blocks

--- tests/cpp/helpers/Log.cpp
+++ tests/cpp/helpers/Log.cpp
@@ -20,7 +20,27 @@
 
 #include <atomic>
 #include <ctime>
+#include <iomanip>
 #include <thread>
+
+static void buf2stream(const void* buf, const size_t size, std::stringstream& ss)
+{
+    int inx = 0;
+
+    ss << std::hex << std::setfill('0') ;
+    do {
+        unsigned int value = static_cast<const unsigned char*>(buf)[inx];
+
+        ss << std::setw(2) << value << " ";
+
+        inx++;
+
+        if (!(inx % 16))
+            ss << std::endl;
+        else if (!(inx % 8))
+            ss << " ";
+    } while (inx < size);
+};
 
 void CLog::Open(const std::string& filename)
 {
@@ -57,6 +77,14 @@ void CLog::Info(const std::stringstream& ss)
         m_out << std::clock() << " " << std::this_thread::get_id() << " " << ss.str() << std::endl;
 };
 
+void CLog::Info(const void* buf, const size_t size)
+{
+    std::stringstream ss;
+
+    buf2stream(buf, size, ss);
+    Info(ss);
+}
+
 void CLog::Err(const char* message)
 {
     std::lock_guard<std::mutex> guard(m_lock);
@@ -84,6 +112,14 @@ void CLog::Err(const std::stringstream& ss)
         m_out << std::clock() << " " << std::this_thread::get_id() << " ERR " << ss.str() << std::endl;
 };
 
+void CLog::Err(const void* buf, const size_t size)
+{
+    std::stringstream ss;
+
+    buf2stream(buf, size, ss);
+    Err(ss);
+}
+
 void CLog::Detail(const char* message)
 {
     std::lock_guard<std::mutex> guard(m_lock);
@@ -107,5 +143,13 @@ void CLog::Detail(const std::stringstream& ss)
     if (m_isOpen)
         m_out << std::clock() << " " << std::this_thread::get_id() << " " << ss.str() << std::endl;
 };
+
+void CLog::Detail(const void* buf, const size_t size)
+{
+    std::stringstream ss;
+
+    buf2stream(buf, size, ss);
+    Detail(ss);
+}
 
 CLog logger;
--- tests/cpp/helpers/Log.h
+++ tests/cpp/helpers/Log.h
@@ -34,14 +34,17 @@ public:
     void Info(const char* message);
     void Info(const std::string& message);
     void Info(const std::stringstream& ss);
+    void Info(const void* buf, const size_t size);
 
     void Err(const char* message);
     void Err(const std::string& message);
     void Err(const std::stringstream& ss);
+    void Err(const void* buf, const size_t size);
 
     void Detail(const char* message);
     void Detail(const std::string& message);
     void Detail(const std::stringstream& ss);
+    void Detail(const void* buf, const size_t size);
 
 private:
     std::mutex m_lock;
