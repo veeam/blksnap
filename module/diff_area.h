12176ca70eeebefba068e0e32f317fc2a0c1a901
Sergei Shtepa <sergei.shtepa@veeam.com>
2022-03-24 11:38:05 +0100

    fixed description for struct diff_area

--- module/diff_area.h
+++ module/diff_area.h
@@ -15,13 +15,12 @@ struct chunk;
 /**
  * struct diff_area - Discribes the difference area for one original device.
  * @kref:
- *	This structure can be shared between &struct tracker and &struct
- *	snapimage.
+ *	The reference counter. The &struct diff_area can be shared between
+ *	&struct tracker and &struct snapimage.
  * @orig_bdev:
  *	A pointer to the structure of an open block device.
  * @diff_storage:
- *	The same &struct diff_area can use the same diff_storage to store
- *	its data.
+ *	Pointer to difference storage for storing difference data.
  * @chunk_shift:
  *	Power of 2 for chunk size. It's allow to set various chunk size for
  *	huge and small block device.
@@ -29,39 +28,39 @@ struct chunk;
  *	Count of chunks. The number of chunks into which the block device
  *	is divided.
  * @chunk_map:
- *	A map of chunk. xarray can be too little on x32 systems. This creates
- *	a limit in the size of supported disks to 256 TB with a chunk size of
- *	65536 bytes. In addition, such a large array will take up too much
- *	space in memory.
- *	Therefore, the size of the chunk should be selected so that the size
- *	of the map is not too large, and the index does not exceed 32 bits.
- * @storing_chunks:
- *	A list of chunks that are waiting to be store sectors from memory to
- *	diff storage.
- *	Reading data from the original block device is performed in the context
- *	of the thread in which the filtering is performed. But storing data to
- *	diff storage is performed in workqueue.
- *	The chunks that need to be stored in diff storage are accumitale in
- *	this list.
- * @storage_list_lock:
- *	Spin lock for the @storing_chunks list.
- * caches_lock:
- *      Spin lock for the @read_cache_queue list.
- * @storing_chunks_work:
- *	The workqueue work item. This worker saves the chunks to the diff
- *	storage.
+ *	A map of chunks.
+ * @in_memory:
+ *	A sign that difference storage is no prepared and all difference are
+ *	stored in RAM.
+ * @caches_lock:
+ *	This spinlock guarantees the consistency of the linked lists of chunks
+ *	caches.
  * @read_cache_queue:
- *	After copying the sectors from the original block device to diff
- *	storage, the sectors are still located in memory. When the snapshot
- *	data is read or written, they also remain in memory for some time.
+ *	Queue for the read cache.
  * @read_cache_count:
- *	The number of chunks in the cache.
+ *	The number of chunks in the read cache.
+ * @write_cache_queue:
+ *	Queue for the write cache.
+ * @write_cache_count:
+ *	The number of chunks in the write cache.
  * @cache_release_work:
- *	The workqueue work item which controls the cache size.
+ *	The workqueue work item. This worker limits the number of chunks
+ *	that store their data in RAM.
+ * @free_diff_buffers_lock:
+ *	This spinlock guarantees the consistency of the linked lists of
+ *	free difference buffers.
+ * @free_diff_buffers:
+ *	Linked list of free difference buffer allows to reduce the number
+ *	of buffer allocation and release operations.
+ * @free_diff_buffers_count:
+ *	The number of free difference buffers in the linked list.
  * @corrupt_flag:
  *	The flag is set if an error occurred in the operation of the data
  *	saving mechanism in the diff area. In this case, an error will be
  *	generated when reading from the snapshot image.
+ * @pending_io_count:
+ *	Counter of incomplete I/O operations. Allows to wait for all I/O
+ *	operations to be completed before releasing this structure.
  *
  * The &struct diff_area is created for each block device in the snapshot.
  * It is used to save the differences between the original block device and
@@ -69,6 +68,29 @@ struct chunk;
  * the differences are copied as chunks to the difference storage.
  * Reading and writing from the snapshot image are also performed using
  * &struct diff_area.
+ *
+ * The xarray has a limit on the maximum size. This can be especially
+ * noticeable on 32-bit systems. This creates a limit in the size of
+ * supported disks.
+ * For example, for a 256 TiB disk with a block size of 65536 bytes, the
+ * number of elements in the chunk map will be equal to two with a power of 32.
+ * Therefore, the number of chunks into which the block device is divided is
+ * limited.
+ *
+ * To provide high performance, a read cache and a write cache for chunks are
+ * used. The cache algorithm is the simplest. If the data of the chunk was
+ * read to the difference buffer, then the buffer is not released immediately,
+ * but is placed at the end of the queue. The worker thread checks the number
+ * of chunks in the queue and release a difference buffer for the first chunk
+ * in the queue, but only if the binary semaphore of the chunk is not locked.
+ * If the read thread accesses the chunk from the cache again, it returns
+ * back to the end of the queue.
+ *
+ * The linked list of difference buffers allows to have a certain number of
+ * "hot" buffers. This allows to reduce the number of allocations and releases
+ * of memory.
+ *
+ *
  */
 struct diff_area {
 	struct kref kref;
