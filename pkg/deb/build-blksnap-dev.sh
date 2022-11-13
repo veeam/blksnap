0fc669953b81605062ce8d77ed2ce177e754b89b
Fabio Fantoni <fantonifabio@tiscali.it>
2022-11-13 20:57:37 +0100

    deb: move out build script to start make a standard debian folder

--- pkg/deb/build-blksnap-dev.sh
+++ pkg/deb/build-blksnap-dev.sh
@@ -8,35 +8,28 @@ else
 fi
 
 CURR_DIR=$(pwd)
-cd "../../../"
+cd "../../"
 ROOT_DIR=$(pwd)
 
 BUILD_DIR="build/pkg"
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 
-SOURCE_DIR="tests"
-TARGET_DIR="opt/blksnap/tests"
-
-# build
-rm -rf ${SOURCE_DIR}/cpp/bin/*
-mkdir -p ${SOURCE_DIR}/cpp/bin/*
-cd ${SOURCE_DIR}/cpp/bin
+# build library
+mkdir -p "lib/blksnap/bin"
+cd "lib/blksnap/bin"
 cmake ../
 make
 cd ${ROOT_DIR}
 
-# copy binaries and scripts
-mkdir -p ${BUILD_DIR}/${TARGET_DIR}
-for FILE in ${SOURCE_DIR}/*
-do
-	if [ -f "${FILE}" ]
-	then
-		cp ${FILE} ${BUILD_DIR}/${TARGET_DIR}/
-	fi
-done
-chmod +x ${BUILD_DIR}/${TARGET_DIR}/*
-INSTALL_FILES+=(${TARGET_DIR}/*)
+# copy library
+mkdir -p ${BUILD_DIR}/usr/lib/
+cp lib/libblksnap.a ${BUILD_DIR}/usr/lib/
+INSTALL_FILES+=("usr/lib/*")
+# copy include
+mkdir -p ${BUILD_DIR}/usr/include/blksnap/
+cp include/blksnap/* ${BUILD_DIR}/usr/include/blksnap/
+INSTALL_FILES+=("usr/include/blksnap/*")
 
 # prepare other package files
 mkdir -p ${BUILD_DIR}/debian
@@ -51,17 +44,18 @@ cat > ${BUILD_DIR}/debian/compat << EOF
 EOF
 
 cat > ${BUILD_DIR}/debian/control << EOF
-Source: blksnap-tests
-Section: utils
+Source: blksnap-dev
+Section: devel
 Priority: optional
 Maintainer: Veeam Software Group GmbH <veeam_team@veeam.com>
-Build-Depends: debhelper (>= 9.0.0), bash, g++, cmake, uuid-dev, libboost-program-options-dev, libboost-filesystem-dev, libssl-dev
+Build-Depends: debhelper (>= 9.0.0), bash, g++, cmake, uuid-dev, libboost-filesystem-dev
 Homepage: https://github.org/veeam/blksnap
 
-Package: blksnap-tests
+Package: blksnap-dev
 Architecture: linux-any
-Depends: blksnap-tools (= ${VERSION}), bash, \${shlibs:Depends}, \${misc:Depends}
-Description: [TBD] The tests for checking the blksnap kernel module.
+Depends: \${misc:Depends}
+Description: [TBD] The static library and header files for managing the
+        blksnap kernel module.
 EOF
 chmod 0666 ${BUILD_DIR}/debian/control
 
@@ -72,26 +66,26 @@ Source: https://github.org/veeam/blksnap
 
 Files: *
 Copyright: 2022 Veeam Software Group GmbH <veeam_team@veeam.com>
-License: GPL-2+
- This package is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
+License: LGPL-3+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Library General Public License as published by the Free
+ Software Foundation; either version 3 of the License, or (at your option) any
+ later version.
  .
- This package is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+ more details.
  .
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  .
- On Debian systems, the complete text of the GNU General Public License
- version 2 can be found in \`/usr/share/common-licenses/GPL-2'.
+ On Debian systems, the complete text of the GNU Library General Public License
+ version 3 can be found in \`/usr/share/common-licenses/LGPL-3'.
 EOF
 
 cat > ${BUILD_DIR}/debian/changelog << EOF
-blksnap-tests (${VERSION}) stable; urgency=low
+blksnap-dev (${VERSION}) stable; urgency=low
 
   * Release.
  -- Veeam Software Group GmbH <veeam_team@veeam.com>  $(date -R)
@@ -120,4 +114,3 @@ done
 dpkg-buildpackage -us -uc -b
 
 cd ${CURR_DIR}
-
