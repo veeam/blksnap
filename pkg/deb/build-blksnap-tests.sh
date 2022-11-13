a4a6686a3474bf0535a6b8360303753626c28ad5
Fabio Fantoni <fantonifabio@tiscali.it>
2022-11-13 22:31:43 +0100

    deb: convert more near to standard debian folder

--- pkg/deb/build-blksnap-tests.sh
+++ pkg/deb/build-blksnap-tests.sh
@@ -36,59 +36,9 @@ do
 	fi
 done
 chmod +x ${BUILD_DIR}/${TARGET_DIR}/*
-INSTALL_FILES+=(${TARGET_DIR}/*)
 
 # prepare other package files
-mkdir -p ${BUILD_DIR}/debian
-mkdir -p ${BUILD_DIR}/debian/source
-
-cat > ${BUILD_DIR}/debian/source/format << EOF
-3.0 (native)
-EOF
-
-cat > ${BUILD_DIR}/debian/compat << EOF
-9
-EOF
-
-cat > ${BUILD_DIR}/debian/control << EOF
-Source: blksnap-tests
-Section: utils
-Priority: optional
-Maintainer: Veeam Software Group GmbH <veeam_team@veeam.com>
-Build-Depends: debhelper (>= 9.0.0), bash, g++, cmake, uuid-dev, libboost-program-options-dev, libboost-filesystem-dev, libssl-dev
-Homepage: https://github.org/veeam/blksnap
-
-Package: blksnap-tests
-Architecture: linux-any
-Depends: blksnap-tools (= ${VERSION}), bash, \${shlibs:Depends}, \${misc:Depends}
-Description: [TBD] The tests for checking the blksnap kernel module.
-EOF
-chmod 0666 ${BUILD_DIR}/debian/control
-
-cat > ${BUILD_DIR}/debian/copyright << EOF
-Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
-Upstream-Name: blksnap
-Source: https://github.org/veeam/blksnap
-
-Files: *
-Copyright: 2022 Veeam Software Group GmbH <veeam_team@veeam.com>
-License: GPL-2+
- This package is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
- .
- This package is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- .
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- .
- On Debian systems, the complete text of the GNU General Public License
- version 2 can be found in \`/usr/share/common-licenses/GPL-2'.
-EOF
+cp -r ${CURR_DIR}/blksnap-tests ${BUILD_DIR}/debian
 
 cat > ${BUILD_DIR}/debian/changelog << EOF
 blksnap-tests (${VERSION}) stable; urgency=low
@@ -97,24 +47,7 @@ blksnap-tests (${VERSION}) stable; urgency=low
  -- Veeam Software Group GmbH <veeam_team@veeam.com>  $(date -R)
 EOF
 
-cat > ${BUILD_DIR}/debian/rules << EOF
-#!/usr/bin/make -f
-# -*- makefile -*-
-include /usr/share/dpkg/pkg-info.mk
-
-%:
-	dh \$@
-
-EOF
-chmod 0766 ${BUILD_DIR}/debian/rules
-
 cd ${BUILD_DIR}
-
-# enumerate installation files
-for FILE in ${INSTALL_FILES[@]}
-do
-	echo "${FILE} "$(dirname ${FILE}) >> debian/install
-done
 
 # build backage
 dpkg-buildpackage -us -uc -b
