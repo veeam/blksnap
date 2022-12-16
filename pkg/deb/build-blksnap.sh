7786fe2746c9f852b9dce06610e2c912c98256a1
Fabio Fantoni <fantonifabio@tiscali.it>
2022-12-16 17:32:19 +0100

    deb: unify in one source package blksnap-dev, blksnap-tools and blksnap tests
    Note: this unify only source packages, binaries package are keeped
    splitted as should be.
    This is another step to have packages more similar to the possible
    official ones.
    I keeped blksnap-dkms out as is better keep separate as it would remain
    only the necessary time while waiting for kernel integration, then it
    would be removed (still talking about possible official packages)

--- pkg/deb/build-blksnap.sh
+++ pkg/deb/build-blksnap.sh
@@ -15,6 +15,38 @@ BUILD_DIR="build/pkg"
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 
+### build library
+mkdir -p "lib/blksnap/bin"
+cd "lib/blksnap/bin"
+cmake ../
+make
+cd ${ROOT_DIR}
+
+# copy library
+mkdir -p ${BUILD_DIR}/usr/lib/
+cp lib/libblksnap.a ${BUILD_DIR}/usr/lib/
+# copy include
+mkdir -p ${BUILD_DIR}/usr/include/blksnap/
+cp include/blksnap/* ${BUILD_DIR}/usr/include/blksnap/
+
+### build tools
+SOURCE_DIR="tools/blksnap/bin"
+TARGET_DIR="usr/sbin"
+
+# build
+rm -rf ${SOURCE_DIR}/*
+mkdir -p ${SOURCE_DIR}
+cd ${SOURCE_DIR}
+cmake ../
+make
+cd ${ROOT_DIR}
+
+# copy binaries
+mkdir -p ${BUILD_DIR}/${TARGET_DIR}
+cp ${SOURCE_DIR}/blksnap ${BUILD_DIR}/${TARGET_DIR}/
+chmod +x ${BUILD_DIR}/${TARGET_DIR}/*
+
+### build tests
 SOURCE_DIR="tests"
 TARGET_DIR="opt/blksnap/tests"
 
@@ -37,11 +69,12 @@ do
 done
 chmod +x ${BUILD_DIR}/${TARGET_DIR}/*
 
-# prepare other package files
-cp -r ${CURR_DIR}/blksnap-tests ${BUILD_DIR}/debian
+
+### prepare other package files
+cp -r ${CURR_DIR}/blksnap ${BUILD_DIR}/debian
 
 cat > ${BUILD_DIR}/debian/changelog << EOF
-blksnap-tests (${VERSION}) stable; urgency=low
+blksnap (${VERSION}) stable; urgency=low
 
   * Release.
  -- Veeam Software Group GmbH <veeam_team@veeam.com>  $(date -R)
@@ -53,4 +86,3 @@ cd ${BUILD_DIR}
 dpkg-buildpackage -us -uc -b
 
 cd ${CURR_DIR}
-
