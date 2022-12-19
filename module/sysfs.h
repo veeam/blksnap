9ba25bf69685c73c25881b7609aaff8d03b4f7a7
Sergei Shtepa <sergei.shtepa@veeam.com>
2022-12-19 11:37:38 +0100

    v2 review - remove sysfs files.
    The misc device model is used instead of the symbolic device model.

--- module/sysfs.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __BLK_SNAP_SYSFS_H
-#define __BLK_SNAP_SYSFS_H
-
-int sysfs_initialize(void);
-void sysfs_finalize(void);
-#endif /* __BLK_SNAP_SYSFS_H */
