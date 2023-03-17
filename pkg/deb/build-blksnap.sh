d4149c1c4664b7c68eb3556d53256c502a7c1f11
Sergei Shtepa <sergei.shtepa@veeam.com>
2023-03-17 18:17:45 +0100

    build performed in 'build' directory
    The build is performed in a directory 'build' specially created for this
    purpose. The result of the build is in the root folder of the project,
    and not in the overlying level.

--- pkg/deb/build-blksnap.sh
+++ pkg/deb/build-blksnap.sh
@@ -10,19 +10,29 @@ fi
 CURR_DIR=$(pwd)
 cd "../../"
 ROOT_DIR=$(pwd)
+BUILD_DIR=${ROOT_DIR}"/build"
+
+# recreate build directory
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+
+# copy sources to build directory
+cp ${ROOT_DIR}"/CMakeLists.txt" ${BUILD_DIR}/
+for SRC_DIR in include lib tests tools
+do
+	cp -r ${ROOT_DIR}"/"${SRC_DIR} ${BUILD_DIR}/
+done
 
 # prepare debian directory
-rm -rf ${ROOT_DIR}/debian
-cp -r ${CURR_DIR}/blksnap ${ROOT_DIR}/debian
-
-cat > ${ROOT_DIR}/debian/changelog << EOF
+cp -r ${CURR_DIR}/blksnap ${BUILD_DIR}/debian
+cat > ${BUILD_DIR}/debian/changelog << EOF
 blksnap (${VERSION}) stable; urgency=low
 
   * Release.
  -- Veeam Software Group GmbH <veeam_team@veeam.com>  $(date -R)
 EOF
 
-cd ${ROOT_DIR}
+cd ${BUILD_DIR}
 
 # build backage
 dpkg-buildpackage -us -uc -b
