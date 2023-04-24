af8ecf7ac7b6e965c8170dbf31708d46e31a7b41
Sergei Shtepa <sergei.shtepa@veeam.com>
2023-04-24 18:14:26 +0200

    create a directory ${TEST_DIR} if it doesn't exist yet

--- tests/4000-diff_storage.sh
+++ tests/4000-diff_storage.sh
@@ -18,8 +18,11 @@ if [ -z $1 ]
 then
 	TEST_DIR=$(realpath ~/blksnap-test)
 else
-	TEST_DIR=$(realpath $1)
+	TEST_DIR=$(realpath $1"/blksnap-test")
 fi
+mkdir -p ${TEST_DIR}
+rm -rf ${TEST_DIR}/*
+
 MP_TEST_DIR=$(stat -c %m ${TEST_DIR})
 DEVICE="/dev/block/"$(mountpoint -d ${MP_TEST_DIR})
 echo "Test directory [${TEST_DIR}] on device [${DEVICE}] selected"
@@ -29,10 +32,8 @@ RELATIVE_TEST_DIR=${TEST_DIR#${MP_TEST_DIR}}
 MP_DIR=/mnt/blksnap-test
 DIFF_STORAGE=${TEST_DIR}/diff_storage/
 
-rm -rf ${TEST_DIR}
 rm -rf ${MP_DIR}
 rm -rf ${DIFF_STORAGE}
-mkdir -p ${TEST_DIR}
 mkdir -p ${MP_DIR}
 mkdir -p ${DIFF_STORAGE}
 
