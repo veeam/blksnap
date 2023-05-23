01cf8f689e870c007d331d124992c0879177f0c4
Sergei Shtepa <sergei.shtepa@veeam.com>
2023-05-23 14:22:55 +0200

    remove unnecessary variable ROOT_DIR

--- pkg/deb/build-blksnap.sh
+++ pkg/deb/build-blksnap.sh
@@ -9,20 +9,17 @@ fi
 
 CURR_DIR=$(pwd)
 cd "../../"
-ROOT_DIR=$(pwd)
 
 # prepare debian directory
-rm -rf ${ROOT_DIR}/debian
-cp -r ${CURR_DIR}/blksnap ${ROOT_DIR}/debian
+rm -rf ./debian
+cp -r ${CURR_DIR}/blksnap ./debian
 
-cat > ${ROOT_DIR}/debian/changelog << EOF
+cat > ./debian/changelog << EOF
 blksnap (${VERSION}) stable; urgency=low
 
   * Release.
  -- Veeam Software Group GmbH <veeam_team@veeam.com>  $(date -R)
 EOF
-
-cd ${ROOT_DIR}
 
 # build backage
 dpkg-buildpackage -us -uc -b
