d01e1a7ca5584763dbbbbdad78b861dba02239f4
Sergei Shtepa <sergei.shtepa@veeam.com>
2024-04-24 17:22:35 +0200

    fix the memory leak in bdevfilter_done()

--- module/bdevfilter.c
+++ module/bdevfilter.c
@@ -442,7 +442,7 @@ static int __init bdevfilter_init(void)
 	ret = register_ftrace_function(&ops_submit_bio_noacct);
 	if (ret) {
 		pr_err("Failed to register ftrace handler (%d)\n", ret);
-		ftrace_set_filter(&ops_submit_bio_noacct, NULL, 0, 1);
+		ftrace_free_filter(&ops_submit_bio_noacct);
 		return ret;
 	}
 
@@ -456,6 +456,7 @@ static void __exit bdevfilter_done(void)
 	struct bdev_extension *ext;
 
 	unregister_ftrace_function(&ops_submit_bio_noacct);
+	ftrace_free_filter(&ops_submit_bio_noacct);
 
 	pr_debug("Ftrace filter for 'submit_bio_noacct' has been unregistered\n");
 
